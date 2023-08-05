7793f9b4d83342d3d956108be51fb973b20e8492
Angle-Brackets <ankitjoju@gmail.com>
2023-08-05 04:32:49 -0500

    Added submodules, so you don't need to download depedencies manually!

--- include/CMakeFiles/Pixelinator.dir/Users/ankitjoju/Desktop/Programs/Side_Projects/Pixelinator/src/engine/render/render_util.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Pixelinator.dir/Users/ankitjoju/Desktop/Programs/Side_Projects/Pixelinator/src/engine/render/render_util.c.o: \
-  /Users/ankitjoju/Desktop/Programs/Side\ Projects/Pixelinator/src/engine/render/render_util.c
--- include/CMakeFiles/Pixelinator.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Pixelinator.
-# This may be replaced when dependencies are built.
