ebafcd97231388b3d429f0ce685b4d79a8f7a193
Angle-Brackets <ankitjoju@gmail.com>
2023-08-05 16:31:58 -0500

    removed submodules again

---
+++ test/sdl2/include/SDL2/SDL_revision.h
@@ -0,0 +1,8 @@
+/* #undef SDL_VENDOR_INFO */
+#define SDL_REVISION_NUMBER 0
+
+#ifdef SDL_VENDOR_INFO
+#define SDL_REVISION "SDL-release-2.28.0-102-gf522c5380 (" SDL_VENDOR_INFO ")"
+#else
+#define SDL_REVISION "SDL-release-2.28.0-102-gf522c5380"
+#endif
